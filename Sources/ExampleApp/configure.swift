b8f2847761f2d12f7907ff6878f74425adbd3b35
Ray Kuo <ray.kuo@sap.com>
2022-08-17 18:12:09 -0700

    remove

--- Sources/ExampleApp/configure.swift
+++ Sources/ExampleApp/configure.swift
@@ -3,7 +3,6 @@ import Twilio
 
 /// Called before your application initializes.
 public func configure(_ app: Application) throws {
-//    app.twilio.configuration = .environment
-    app.twilio.configuration = .init(accountId: "AC37be6f46dbdaca1fbf7d97edbc105284", accountSecret: "d0e12e54c0870932b64cf3a564677c6f")
+    app.twilio.configuration = .environment
     try routes(app)
 }
