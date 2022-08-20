05db03bac72628ae3a3f3f71eab0d00f1e0748a4
Ray Kuo <imentos@gmail.com>
2022-08-19 18:06:12 -0700

    Develop (#2)
    * add twilio call
    
    * remove
    
    * remove comments
    
    * use vapor async
    
    * add status call back
    
    Co-authored-by: Ray Kuo <ray.kuo@sap.com>

--- Sources/ExampleApp/routes.swift
+++ Sources/ExampleApp/routes.swift
@@ -3,21 +3,18 @@ import Twilio
 
 /// Register your application's routes here.
 func routes(_ app: Application) throws {
-    // Basic "It works" example
-    //(831) 610-0806
-    app.get { req -> EventLoopFuture<ClientResponse> in
-//        let sms = OutgoingSMS(body: "Hey There", from: "+14054496618", to: "+16508478622")
-//
-//        return req.twilio.send(sms)
-        
-        let call = OutgoingCall(twiml: "<Response><Say>This is Ray</Say></Response>", from: "+14054496618", to: "+16508478622")
-//        let call = OutgoingCall(url: "http://demo.twilio.com/docs/voice.xml", from: "+14054496618", to: "+16508478622")
-        return req.twilio.send(call)
+
+    app.get { req async throws -> ClientResponse in
+        let call = OutgoingCall(
+            twiml: "<Response><Say>This is Ray</Say></Response>",
+            from: "+14054496618",
+            to: "+16508478622",
+            statusCallback: "https://1c2d-169-145-42-40.ngrok.io/status")
+        return try await req.twilio.send(call)
     }
 
     app.post("incoming") { req -> Response in
         let sms = try req.content.decode(IncomingSMS.self)
-
         let responseMessage = SMSResponse(
             Message(body: "Hello Friend!"),
             Message(body: "This is a second text.")
@@ -25,6 +22,16 @@ func routes(_ app: Application) throws {
 
         return req.twilio.respond(with: responseMessage)
     }
+        
+    app.post("status") { req async throws -> Response in
+        let status = try req.content.decode(StatusCallback.self)
+        print(status.CallStatus)
+        return Response(status: .ok)
+    }
+}
+
+struct StatusCallback: Content {
+    var CallStatus: String
 }
 
 
