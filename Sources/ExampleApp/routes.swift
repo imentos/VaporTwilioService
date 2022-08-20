c70f227f758e8d1dbfa4e987fd1069309cf6505d
Ray Kuo <ray.kuo@sap.com>
2022-08-19 17:35:42 -0700

    add status call back

--- Sources/ExampleApp/routes.swift
+++ Sources/ExampleApp/routes.swift
@@ -4,7 +4,11 @@ import Twilio
 /// Register your application's routes here.
 func routes(_ app: Application) throws {
     app.get { req async throws -> ClientResponse in
-        let call = OutgoingCall(twiml: "<Response><Say>This is Ray</Say></Response>", from: "+14054496618", to: "+16508478622")
+        let call = OutgoingCall(
+            twiml: "<Response><Say>This is Ray</Say></Response>",
+            from: "+14054496618",
+            to: "+16508478622",
+            statusCallback: "https://1c2d-169-145-42-40.ngrok.io/status")
         return try await req.twilio.send(call)
     }
 
@@ -17,6 +21,16 @@ func routes(_ app: Application) throws {
 
         return req.twilio.respond(with: responseMessage)
     }
+        
+    app.post("status") { req async throws -> Response in
+        let status = try req.content.decode(StatusCallback.self)
+        print(status.CallStatus)
+        return Response(status: .ok)
+    }
+}
+
+struct StatusCallback: Content {
+    var CallStatus: String
 }
 
 
--- Sources/Twilio/Models/OutgoingCall.swift
+++ Sources/Twilio/Models/OutgoingCall.swift
@@ -11,14 +11,16 @@ public struct OutgoingCall: Content {
     let twiml: String
     let from: String
     let to: String
+    let statusCallback: String
 
-    public init(twiml: String, from: String, to: String) {
+    public init(twiml: String, from: String, to: String, statusCallback: String) {
         self.twiml = twiml
         self.from = from
         self.to = to
+        self.statusCallback = statusCallback
     }
 
     private enum CodingKeys : String, CodingKey {
-        case twiml = "Twiml", from = "From", to = "To"
+        case twiml = "Twiml", from = "From", to = "To", statusCallback = "StatusCallback"
     }
 }
